5187b28d23c2151fd0d73101420ba9f6e0525d66
git-abangar <akashbangar8776@gmail.com>
2024-03-16 16:53:41 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Made a custom logging to monitor on cloud platform")
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ =='__main__':
+    try :
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<< \n\nx==================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
